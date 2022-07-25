fbd7f67eb9f593a80c441463f10733554323c252
Thomas Scott <tscott@digitalocean.com>
2022-07-25 12:43:28 -0400

    Corrected version number to remove pre-pended "v"

--- Casks/notable-insiders.rb
+++ Casks/notable-insiders.rb
@@ -1,5 +1,5 @@
 cask "notable-insiders" do
-  version "v1.9.0-beta.9"
+  version "1.9.0-beta.9"
   sha256 "ae8f279d25befbb71f673eb279e7c8f12bc0503518c2501894e6e69d0c92b68c"
 
   url "https://github.com/notable/notable-insiders/releases/download/v#{version}/Notable-#{version}-universal.dmg",
